a9dce19007f077e721a6b35610411090148ae499
Tim Satrya <timsatrya@gmail.com>
2021-05-22 01:41:50 +0200

    TDXM-97
    das problem ist warscheinlich beim new OnCompleteListener<AuthResult>()

--- tdm-food-tracker-project/app/src/main/java/com/example/tdm_food_tracker/activities/RegisteringActivity.java
+++ tdm-food-tracker-project/app/src/main/java/com/example/tdm_food_tracker/activities/RegisteringActivity.java
@@ -1,7 +1,7 @@
 package com.example.tdm_food_tracker.activities;
 
 import android.os.Bundle;
-import android.util.Patterns;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -12,11 +12,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tdm_food_tracker.R;
 import com.example.tdm_food_tracker.user.User;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+
 
 public class RegisteringActivity extends AppCompatActivity {
 
@@ -37,76 +39,93 @@ public class RegisteringActivity extends AppCompatActivity {
         userPasswordConfirm = (EditText) findViewById(R.id.userPasswordConfirmInput);
 
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+
     }
 
-    private void registerUser(){
+    public void registerUser(View v){
         String name = userName.toString().trim();
         String email = userEmail.toString().trim();
         String password = userPassword.toString().trim();
         String confirmpassword = userPasswordConfirm.toString().trim();
 
         if(name.isEmpty()){
-            userName.setError(""+R.string.field_not_filled_out);
+            userName.setError(getString(R.string.field_not_filled_out));
             userName.requestFocus();
             return;
         }
 
         if(email.isEmpty()){
-            userEmail.setError(""+R.string.field_not_filled_out);
+            userEmail.setError(getString(R.string.field_not_filled_out));
             userEmail.requestFocus();
             return;
         }
 
         if(password.isEmpty()){
-            userPassword.setError(""+R.string.field_not_filled_out);
+            userPassword.setError(getString(R.string.field_not_filled_out));
             userPassword.requestFocus();
             return;
         }
 
         if(confirmpassword.isEmpty()){
-            userPasswordConfirm.setError(""+R.string.field_not_filled_out);
+            userPasswordConfirm.setError(getString(R.string.field_not_filled_out));
             userPasswordConfirm.requestFocus();
             return;
         }
-        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-            userEmail.setError(""+R.string.email_pattern_error);
+       /* if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            userEmail.setError(getString(R.string.email_pattern_error));
             userEmail.requestFocus();
             return;
         }
 
+        */
+
         if(password.length() < 6){
-            userPasswordConfirm.setError(""+R.string.password_to_short);
+            userPasswordConfirm.setError(getString(R.string.password_to_short));
             userPasswordConfirm.requestFocus();
             return;
 
         }
 
+        Log.d("nya", "toast 2: ");
+
+
         progressBar.setVisibility(View.VISIBLE);
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+
                     @Override
-                    public void onComplete(@NonNull @org.jetbrains.annotations.NotNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
                             User user = new User(name, email);
+                            Log.d("nya", "toast 1: ");
 
                             FirebaseDatabase.getInstance().getReference("Users")
                                     .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
                                     .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+
                                 @Override
-                                public void onComplete(@NonNull @org.jetbrains.annotations.NotNull Task<Void> task) {
-                                    if(task.isSuccessful()){
-                                        Toast.makeText(RegisteringActivity.this, R.string.user_has_been_registered_successfully,Toast.LENGTH_LONG).show();
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Log.d("nya", "toast: ");
+                                        Toast.makeText(RegisteringActivity.this, R.string.user_has_been_registered_successfully, Toast.LENGTH_LONG).show();
                                         progressBar.setVisibility(View.VISIBLE);
-                                    }
-                                    else{
-                                        Toast.makeText(RegisteringActivity.this, "Faild to register! Please try again.",Toast.LENGTH_LONG).show();
+                                    } else {
+                                        Toast.makeText(RegisteringActivity.this, "Faild to register! Please try again.", Toast.LENGTH_LONG).show();
                                         progressBar.setVisibility(View.VISIBLE);
                                     }
                                 }
                             });
+
+                        } else {
+                            Toast.makeText(RegisteringActivity.this, R.string.user_has_been_registered_failed, Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
                         }
                     }
                 });
+
 
     }
 
