7d368298e4fffa1b43e4a2b21f11945273f7cb32
Tim Satrya <timsatrya@gmail.com>
2021-06-01 12:21:06 +0200

    Merge Log in with develop

---
+++ Mealstock/app/src/main/java/com/example/tdm_food_tracker/user/User.java
@@ -0,0 +1,20 @@
+package com.example.tdm_food_tracker.user;
+
+public class User {
+
+    public String name, email;
+
+    public User(){
+
+    }
+
+    public User(String name, String email){
+        this.name = name;
+        this.email = email;
+
+
+    }
+
+
+
+}
