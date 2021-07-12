c78b5cf3bb418fdd65593b337692ad8ea881ee0d
s80464 <s80464@beuth-hochschule.de>
2021-07-12 16:01:44 +0200

    [TDXM-194, F] Splashscreen implemented, background picture with fruits and veggies a little pixelish

--- Mealstock/app/src/main/java/com/example/mealstock/activities/GoogleSignInActivity.java
+++ Mealstock/app/src/main/java/com/example/mealstock/activities/GoogleSignInActivity.java
@@ -44,7 +44,7 @@ public class GoogleSignInActivity extends Activity {
         // [START config_signin]
         // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
+               // .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
